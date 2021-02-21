55fcd7c0b5ad6329e50173089f5f83d1d7f7758c
Yasir Nacak <yasir.nacak@gmail.com>
2021-02-21 12:00:51 +0300

    187: Sequelize database created

---
+++ models/user.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      name: DataTypes.STRING,
+      lastname: DataTypes.STRING,
+      department: DataTypes.STRING,
+      year: DataTypes.INTEGER,
+      email: DataTypes.STRING,
+      studentId: DataTypes.STRING,
+      phone: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
