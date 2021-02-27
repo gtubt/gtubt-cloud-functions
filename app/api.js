0ba83a6d72a5979fc6e463136a7d3c7ddd057fd9
Yasir Nacak <yasir.nacak@gmail.com>
2021-02-27 20:34:40 +0300

    187: User endpoint renamed Users

--- app/api.js
+++ app/api.js
@@ -2,6 +2,6 @@ const express = require("express");
 const users_router = require("./routes/users/users-router");
 
 const api = express();
-api.use("/user", users_router);
+api.use("/users", users_router);
 
 module.exports = api;
