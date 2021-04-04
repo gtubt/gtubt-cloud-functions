c6fe07344f53910b85ad2740f739994fbea46dba
Mehmet Gürol Çay <m.gurolcay@gmail.com>
2021-04-04 12:00:51 +0300

    214: middleware update

--- app/routes/events/events-router.js
+++ app/routes/events/events-router.js
@@ -9,7 +9,7 @@ const json_parser = body_parser.json();
 
 event_router.param("event", events_route_param_handlers.event_param_handler);
 
-event_router.all("/all/:id?", middlewares.restrict_to_admin, json_parser, events_controller.get_all_events);
+event_router.all("/all/:id?", middlewares.restrict_to_self, json_parser, events_controller.get_all_events);
 event_router.get("/:event", middlewares.restrict_to_self, json_parser, events_controller.get_event);
 event_router.post("/", middlewares.restrict_to_admin, json_parser, events_controller.create_event);
 event_router.patch("/:event", middlewares.restrict_to_admin, json_parser, events_controller.update_event);
