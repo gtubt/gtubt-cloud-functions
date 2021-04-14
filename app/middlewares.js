79dc9049d97de3cd11d3a7988a6d864ade566168
Ali Karadogan <akaradogan@outlook.com>
2021-04-14 18:21:37 +0300

    246: fix variable name for firebase token, fix error message

--- app/middlewares.js
+++ app/middlewares.js
@@ -10,13 +10,13 @@ function restrict_to_self(req, res, next) {
   const firebaseToken = req.get("x-firebase-header");
   admin
     .auth()
-    .verifyIdToken(idToken)
+    .verifyIdToken(firebaseToken)
     .then((decodedToken) => {
       req.uid = decodedToken;
       next();
     })
     .catch((error) => {
-      res.status(404).json(utils.get_response_object(null, `Unable to verify ID token. Reason: ${error.message}`, 404));
+      res.status(404).json(utils.get_response_object(null, `Unable to verify ID token. Reason: ${error}`, 404));
     });
 }
 
