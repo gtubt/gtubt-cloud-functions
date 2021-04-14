bafad826ddfa0274e65a5b6de9c48d32d78569ca
Ali Karadogan <akaradogan@outlook.com>
2021-04-14 14:08:49 +0300

    246: add firebase token verification

--- app/middlewares.js
+++ app/middlewares.js
@@ -1,11 +1,23 @@
+const admin = require("firebase-admin");
+const utils = require("./utils");
+
 function restrict_to_admin(req, res, next) {
   // TODO: Implement
   next();
 }
 
 function restrict_to_self(req, res, next) {
-  // TODO: Implement
-  next();
+  const firebaseToken = req.get("x-firebase-header");
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.uid = decodedToken;
+      next();
+    })
+    .catch((error) => {
+      res.status(404).json(utils.get_response_object(null, `Unable to verify ID token. Reason: ${error.message}`, 404));
+    });
 }
 
 module.exports = {
