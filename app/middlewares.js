304857c6f1674268d1a89fb5685ceae8a71d3bec
Ali Karadogan <51951334+alikaradogan@users.noreply.github.com>
2021-04-17 11:35:44 +0300

    246: add firebase token verification (#12)
    * 246: add firebase token verification
    
    * 246: fix variable name for firebase token, fix error message
    
    * 246: Change token header name, change verification error return status to 401 (Unauthorized)
    
    * 246: add firebase-admin to package

--- app/middlewares.js
+++ app/middlewares.js
@@ -1,11 +1,23 @@
+const admin = require("firebase-admin");
+const utils = require("./utils");
+
 function restrict_to_admin(req, res, next) {
   // TODO: Implement
   next();
 }
 
 function restrict_to_self(req, res, next) {
-  // TODO: Implement
-  next();
+  const firebaseToken = req.get("x-firebase-token");
+  admin
+    .auth()
+    .verifyIdToken(firebaseToken)
+    .then((decodedToken) => {
+      req.uid = decodedToken;
+      next();
+    })
+    .catch((error) => {
+      res.status(401).json(utils.get_response_object(null, `User Unauthorized: Unable to verify ID token. Reason: ${error}`, 401));
+    });
 }
 
 module.exports = {
