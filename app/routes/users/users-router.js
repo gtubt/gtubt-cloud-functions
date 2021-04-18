aa18cb3a3481365b320cd205ee08a1f1dd559016
Yasir Nacak <yasir.nacak@gmail.com>
2021-04-18 16:59:27 +0300

    Middleware to check and ensure user. (#14)
    * Middleware to check and ensure user.
    
    * Middleware added to register, validate adjusted for it.
    
    * Check for firebase token in registration.

--- app/routes/users/users-router.js
+++ app/routes/users/users-router.js
@@ -11,10 +11,10 @@ const raw_parser = body_parser.raw({ type: "image/*", limit: "1mb" });
 user_router.param("user", users_route_param_handlers.user_param_handler);
 
 user_router.all("/all/:email?", middlewares.restrict_to_admin, json_parser, users_controller.get_all_users);
-user_router.get("/:user", middlewares.restrict_to_self, json_parser, users_controller.get_user);
-user_router.post("/", middlewares.restrict_to_self, json_parser, users_controller.create_user);
-user_router.post("/:user/photo", middlewares.restrict_to_self, raw_parser, users_controller.upload_user_photo);
-user_router.patch("/:user", middlewares.restrict_to_self, json_parser, users_controller.update_user);
-user_router.delete("/:user", middlewares.restrict_to_self, json_parser, users_controller.delete_user);
+user_router.get("/:user", middlewares.validate_user, middlewares.restrict_to_self, json_parser, users_controller.get_user);
+user_router.post("/", json_parser, users_controller.create_user);
+user_router.post("/:user/photo", middlewares.validate_user, middlewares.restrict_to_self, raw_parser, users_controller.upload_user_photo);
+user_router.patch("/:user", middlewares.validate_user, middlewares.restrict_to_self, json_parser, users_controller.update_user);
+user_router.delete("/:user", middlewares.validate_user, middlewares.restrict_to_self, json_parser, users_controller.delete_user);
 
 module.exports = user_router;
